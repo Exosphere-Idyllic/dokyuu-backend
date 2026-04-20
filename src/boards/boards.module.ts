da6243cccc7d15438f72a36b836c0e53d82f8d08
nad <pan.tostado.ascii@gmail.com>
2026-04-19 22:52:06 -0500

    feat: create BoardsModule with service, controller, and Mongoose schema integration

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,16 +4,18 @@ import { BoardsController } from './boards.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Board, BoardSchema } from '../schemas/board.schema';
 import { BoardMember, BoardMemberSchema } from '../schemas/board-member.schema';
+import { BoardElement, BoardElementSchema } from '../schemas/board-element.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Board.name, schema: BoardSchema },
-      { name: BoardMember.name, schema: BoardMemberSchema }
+      { name: BoardMember.name, schema: BoardMemberSchema },
+      { name: BoardElement.name, schema: BoardElementSchema },
     ])
   ],
   providers: [BoardsService],
   controllers: [BoardsController],
   exports: [BoardsService]
 })
-export class BoardsModule {}
+export class BoardsModule { }
